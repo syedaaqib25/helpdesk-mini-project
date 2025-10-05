b75799d4f56b6c08965f021bf376cbc07ac20028
Syed Aaqib <syedaqib2742@gmail.com>
2025-10-05 21:31:24 +0530

    Update API URL for deployment

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://helpdesk-mini-project.onrender.com',
 });
 
 API.interceptors.request.use((req) => {
