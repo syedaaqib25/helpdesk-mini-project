30c73f955c198066b814a1a8473a8df3a027e207
Syed Aaqib <syedaqib2742@gmail.com>
2025-10-05 23:01:16 +0530

    fixed

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://helpdesk-mini-project.onrender.com',
+  baseURL: import.meta.env.VITE_API_URI,
 });
 
 API.interceptors.request.use((req) => {
